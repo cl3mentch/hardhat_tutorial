183766ae27c02c13b27426a925a67b1cf73d42f7
clement <chienher@hotmail.com>
2024-07-08 22:57:21 +0800

    made some changes to erc20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import Secret from './secret.json'
+import "@nomicfoundation/hardhat-verify";
+import Secret from "./secret.json";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
   networks: {
     bscTestnet: {
       url: Secret.NETWORK_RPC_URL,
-      accounts: [`0x${Secret.PRIVATE_KEY}`]
+      accounts: [`0x${Secret.PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: Secret.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
   },
 };
 
