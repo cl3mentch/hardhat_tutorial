41fa19826f9f6a65d6ec9be3160fc884f402fcb3
clement <chienher@hotmail.com>
2024-07-08 19:24:31 +0800

    .

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require('dotenv').config({path:__dirname+'./.env'})
+import Secret from './secret.json'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
   networks: {
     bscTestnet: {
-      url: process.env.NETWORK_RPC_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      url: Secret.NETWORK_RPC_URL,
+      accounts: [`0x${Secret.PRIVATE_KEY}`]
     },
   },
 };
